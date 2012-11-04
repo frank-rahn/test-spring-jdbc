2e3e58db078c0b28bb576709ac322c041cb6a0dd
Frank W. Rahn <frank@frank-rahn.de>
2012-11-04 18:01:31 +0100

    Umstrukturiert

--- src/sql/postgresql/init.sql
+++ src/sql/postgresql/init.sql
@@ -1,32 +1,32 @@
 DROP FUNCTION IF EXISTS searchPersons(
-	INTEGER, s_User 
+	INTEGER, s_User
 ) CASCADE;
 DROP TYPE IF EXISTS s_User CASCADE;
 DROP TYPE IF EXISTS s_Person CASCADE;
 
 CREATE TYPE s_User AS ( -- Der aktuelle Benutzer
-	id CHAR(10),
-	name VARCHAR(30),
-	dept VARCHAR
+	id		CHAR(10),
+	name	VARCHAR(30),
+	dept	VARCHAR
 );
 
 CREATE TYPE s_Person AS ( -- Eine Person
-	id BIGINT,
-	name VARCHAR(30),
-	salary DECIMAL,
-	dateOfBirth DATE
+	id			BIGINT,
+	name		VARCHAR(30),
+	salary		DECIMAL,
+	dateOfBirth	DATE
 );
 
 CREATE FUNCTION searchPersons( -- Suche Personen
-	IN p_num INTEGER, 
-	IN p_user s_User, 
-	OUT p_persons s_Person[]
+	p_num		IN	INTEGER,
+	p_user		IN	s_User,
+	p_persons	OUT	s_Person[]
 ) AS
 $BODY$  -- Beginn der PL/pgSQL Funktion
 	DECLARE -- Deklarationsblocko
-		i INTEGER;
-		p s_Person;
-		dateOfBirth DATE := current_date;
+		i			INTEGER;
+		p			s_Person;
+		dateOfBirth	DATE := current_date;
 	BEGIN -- Ausführungsteil
 		RAISE NOTICE 'Die Stored Procedure searchPersons wurde aufgerufen. User=%',
 			p_user;
