9cec24faaa1884b03100df44645a3058e99fd202
Frank W. Rahn <frank@frank-rahn.de>
2013-01-04 23:41:28 +0100

    Zweiten Test geschrieben

--- src/test/de/rahn/jdbc/ConnectionTest.java
+++ src/test/de/rahn/jdbc/ConnectionTest.java
@@ -1,12 +1,18 @@
 package de.rahn.jdbc;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Struct;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.ConnectionCallback;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,17 +28,35 @@ public class ConnectionTest {
 	@Autowired
 	private JdbcTemplate template;
 
-	@Value("#{jdbc.testquery}")
+	@Value("${jdbc.testquery}")
 	private String query;
 
 	/**
-	 * Teste die Datenbankverbindung.
+	 * Teste die Datenbankverbindung mit einer Testabfrage.
 	 */
 	@Test
-	public void testDatabaseConnection() {
+	public void testDatabaseConnectionPerQuery() {
 		int result = template.queryForInt(query);
 
 		assertThat("Testquery '" + query + "' fehlgeschlagen", result, is(1));
+	}
+
+	/**
+	 * Teste die Datenbankverbindung über die JDBC-API.
+	 */
+	@Test
+	public void testDatabaseConnectionPerApi() {
+		Object result = template.execute(new ConnectionCallback<Object>() {
+
+			@Override
+			public Object doInConnection(Connection con) throws SQLException {
+				return con.createStruct("s_User", null);
+			}
+
+		});
+
+		assertThat("Die Datenbankaktion lieferte keine Struktur", result,
+			instanceOf(Struct.class));
 	}
 
 }
